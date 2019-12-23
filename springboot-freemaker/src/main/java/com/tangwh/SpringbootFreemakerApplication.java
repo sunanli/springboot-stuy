0bd710c2aa860fffb09f121172ad4c0f4ad2abad
howes <702640991@qq.com>
2019-12-23 16:38:53 +0800

    创建Freemaker模板引擎

---
+++ springboot-freemaker/src/main/java/com/tangwh/SpringbootFreemakerApplication.java
@@ -0,0 +1,13 @@
+package com.tangwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFreemakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFreemakerApplication.class, args);
+    }
+
+}
