3b25c1c2614e6a5294eca9924456708805a41a68
small-howe <2678691035@qq.com>
2019-12-26 14:23:35 +0800

    Json

--- springboot-json/src/main/java/com/tangwh/controller/UserController.java
+++ springboot-json/src/main/java/com/tangwh/controller/UserController.java
@@ -1,10 +1,36 @@
 package com.tangwh.controller;
 
-public class UserController
-{
+import com.tangwh.pojo.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//@Controller
+@RestController //= @ResponseBody+@Controller
+public class UserController {
+
+
+    //@ResponseBody:返回JSON数据
+    @GetMapping("/user")
+    public List<User> getAllUser(){
+
+        List<User> users = new ArrayList<>();
+        for (int i =0;i<10;i++) {
+            User user = new User();
+            user.setId(i);
+            user.setName("小明"+i);
+            user.setAddress("上海"+i);
+            users.add(user);
+        }
+        return users;
+    }
+
+
 }
-
-
 
 
 
--- springboot-json/src/main/java/com/tangwh/pojo/User.java
+++ springboot-json/src/main/java/com/tangwh/pojo/User.java
@@ -1,8 +1,18 @@
 package com.tangwh.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class User {
+
     private Integer id;
+
     private String name;
 
     private String address;
