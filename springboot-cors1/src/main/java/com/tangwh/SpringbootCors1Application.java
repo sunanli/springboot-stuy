bc738e239bfefcf2ec3172d4852b6bf85fccaaba
small-howe <2678691035@qq.com>
2019-12-27 15:19:44 +0800

    使用Cors实现跨域 提供者

---
+++ springboot-cors1/src/main/java/com/tangwh/SpringbootCors1Application.java
@@ -0,0 +1,13 @@
+package com.tangwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCors1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCors1Application.class, args);
+    }
+
+}
---
+++ springboot-cors1/src/main/java/com/tangwh/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.tangwh.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    @GetMapping("/hello")
+    public String hello(){
+
+
+        return "Hello Cors";
+    }
+}
