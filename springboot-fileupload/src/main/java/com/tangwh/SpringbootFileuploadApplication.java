6d88f5f6e652da25c706322685a30413c2aeaaf6
small-howe <2678691035@qq.com>
2019-12-27 10:33:54 +0800

    实现单,多,文件上传

---
+++ springboot-fileupload/src/main/java/com/tangwh/SpringbootFileuploadApplication.java
@@ -0,0 +1,13 @@
+package com.tangwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFileuploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFileuploadApplication.class, args);
+    }
+
+}
