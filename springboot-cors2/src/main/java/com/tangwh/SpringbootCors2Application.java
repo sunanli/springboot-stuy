5002e515aed4e5972f97a421f610db9dcebecbc8
small-howe <2678691035@qq.com>
2019-12-27 16:51:34 +0800

    实现跨域 消费者

---
+++ springboot-cors2/src/main/java/com/tangwh/SpringbootCors2Application.java
@@ -0,0 +1,13 @@
+package com.tangwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCors2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCors2Application.class, args);
+    }
+
+}
