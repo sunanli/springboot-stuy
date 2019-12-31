c2e71eb07b96bbea55b2b6e86c9443dcc04a3035
small-howe <2678691035@qq.com>
2019-12-31 16:56:08 +0800

    集成jdbcTemplate多数据源配置

--- springboot-jdbctemplate/src/main/java/com/tangwh/controller/UserOneJdbcController.java
+++ springboot-jdbctemplate/src/main/java/com/tangwh/controller/UserOneJdbcController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.sound.midi.Soundbank;
 import java.util.List;
 
 @RestController
