bfa77b8213cd8ba1fcac0be0366e96c45b3afa6e
samll-howe <2678691035@qq.com>
2020-01-07 12:48:13 +0800

    Hello World

---
+++ springboot-hello/src/main/java/com/tangwh/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.tangwh.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Tangweihao
+ * @version 1.0
+ * @date 2020/1/7 11:27
+ */
+@RestController
+public class HelloController {
+
+    @PostMapping("/info")
+    public String getInfo(@RequestParam("name") String info){
+
+        System.out.println(info);
+        return info;
+    }
+}
