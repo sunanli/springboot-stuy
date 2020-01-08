bfdb4b3030bee7022572e4262a2e2308ebc55760
small-howe <2678691035@qq.com>
2020-01-08 13:07:44 +0800

    创建工程

---
+++ springboot-cache-redis/src/test/java/com/tangwh/SpringbootCacheRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tangwh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCacheRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
