8e131dcd291291fc546afdf2b6edaca9a04fbde7
samll-howe <2678691035@qq.com>
2020-01-21 11:35:56 +0800

    邮件发送 之 网易邮箱

--- springboot-email/src/main/java/com/tangwh/SpringbootEmailApplication.java
+++ springboot-email/src/main/java/com/tangwh/SpringbootEmailApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootEmailApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootEmailApplication.class, args);
     }
